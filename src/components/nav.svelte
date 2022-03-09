b5abfbfe361cddeb2d7c8aa26420c033505194b6
damtzi <damian.tziamtzis@gmail.com>
2022-03-09 23:20:41 +0000

    chore: Add logo to navbar

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,7 +1,11 @@
-<header class="w-full text-white backdrop-filter backdrop-blur-md sticky top-0 z-10 py-6 px-8 lg:px-24 xl:px-48">
+<script>
+    import Logo from '../../static/malcons-no-bg.png';
+</script>
+
+<header class="w-full text-white backdrop-filter backdrop-blur-md sticky top-0 z-10 px-8 lg:px-24 xl:px-48">
 	<div class="grid grid-cols-2 md:grid-cols-12 md:gap-8">
 		<div class="md:col-span-3 flex items-center justify-between">
-			<h2 class="text-md">Logo</h2>
+            <img src={Logo} alt="Malcons" />
 		</div>
 		<div class="md:col-span-9 items-center flex justify-end flex-row">
 			<a href="/" class="mx-4 text-md">Home</a>
