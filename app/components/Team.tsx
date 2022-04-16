9aafcbce93e644aa21a7bc000567e0240d23465b
damtzi <damian.tziamtzis@gmail.com>
2022-04-16 20:57:21 +0200

    chore: Fix statis asset patch when deployed

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -1,7 +1,7 @@
 const Team = () => {
     return (
         <div className="w-full py-8 px-8 lg:px-24 xl:px-48">
-            <h1 className="text-xl text-white">Team Malcons</h1>
+            <h1 className="text-xl text-white">Team</h1>
         </div>
     );
 };
--- remix.config.js
+++ remix.config.js
@@ -6,5 +6,5 @@ module.exports = {
     // appDirectory: "app",
     assetsBuildDirectory: "public/build",
     serverBuildPath: "build/index.js",
-    publicPath: "/build/",
+    publicPath: "/website/build/",
 };
