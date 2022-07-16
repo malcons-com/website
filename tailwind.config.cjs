7060ab14efa35f7cb7662a938d3804116a95a4c4
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2022-07-16 18:44:49 +0200

    feat: Add a basic skeleton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'navy-blue': '#000069',
+                'azure-blue': '#007FFF',
+                'canary-yellow': '#FEEC02'
+            }
+        }
     },
     plugins: [],
 }
